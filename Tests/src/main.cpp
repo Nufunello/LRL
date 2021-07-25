d353b11b46095a84ba2d8765c10aadbb532052be
Borys Panchenko-Vahner <nafania1122@gmail.com>
2021-07-25 23:46:29 +0300

    Removed experimental tests

--- Tests/src/main.cpp
+++ Tests/src/main.cpp
@@ -1,15 +1,5 @@
 #include <gtest/gtest.h>
 
-TEST(HelloWorld, Positive)
-{
-  EXPECT_EQ(7 * 6, 42);
-}
-
-TEST(HelloWorld, Negative)
-{
-  EXPECT_EQ(7 * 6 + 1, 42);
-}
-
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv); 
