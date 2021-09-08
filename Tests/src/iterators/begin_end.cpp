ea7750fdd477404229449eac7af278b6b1d0ba3d
Borys Panchenko-Vahner <nafania578@gmail.com>
2021-09-08 11:38:23 +0300

    Remove unused test class and split "BeginEnd" test

--- Tests/src/iterators/begin_end.cpp
+++ Tests/src/iterators/begin_end.cpp
@@ -23,33 +23,34 @@ namespace
 		return areInvokeResultsTypeSame<Lambda1, Lambda2, 
 			T, std::add_rvalue_reference_t<T>,
 			std::add_const_t<std::add_lvalue_reference_t<T>>, std::add_lvalue_reference_t<T>>();
-	}	
-	class IteratorsBeginEnd 
-		: public ::testing::Test 
-	{	
-	};	
-	TEST_F(IteratorsBeginEnd, VectorIterators)
+	}
+
+	TEST(Iterators, Begin)
 	{
 		using vector_type = std::vector<int>;
-		{
-			constexpr bool beginSameType = isInvokeResultTypeSame<vector_type>(
-				  [](auto&& vec){ return std::begin(vec); }
-				, [](auto&& vec){ return lrl::iterators::begin(vec); }
-			);
-			EXPECT_TRUE(beginSameType)
-				<< "calls of functions \"begin\" should return same type";
-		}	
-		{
-			constexpr bool endSameType = isInvokeResultTypeSame<vector_type>(
-				  [](auto&& vec){ return std::end(vec); }
-				, [](auto&& vec){ return lrl::iterators::end(vec); }
-			);
-			EXPECT_TRUE(endSameType)
-				<< "calls of functions \"end\" should return same type";
-		}	
+		constexpr bool beginSameType = isInvokeResultTypeSame<vector_type>(
+			  [](auto&& vec){ return std::begin(vec); }
+			, [](auto&& vec){ return lrl::iterators::begin(vec); }
+		);
+		EXPECT_TRUE(beginSameType)
+			<< "calls of functions \"begin\" should return same type";
+
 		const vector_type vector = {1, 2, 3, 4, 5, 6};
 		EXPECT_EQ(std::begin(vector), lrl::iterators::begin(vector)) 
 			<< "calls of functions \"begin\" are not equal for vector";	
+	}
+
+	TEST(Iterators, End)
+	{
+		using vector_type = std::vector<int>;
+		constexpr bool endSameType = isInvokeResultTypeSame<vector_type>(
+			  [](auto&& vec){ return std::end(vec); }
+			, [](auto&& vec){ return lrl::iterators::end(vec); }
+		);
+		EXPECT_TRUE(endSameType)
+			<< "calls of functions \"end\" should return same type";
+
+		const vector_type vector = {1, 2, 3, 4, 5, 6};
 		EXPECT_EQ(std::end(vector), lrl::iterators::end(vector))
 			<< "calls of functions \"end\" are not equal for vector";
 	}
