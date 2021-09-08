498d51975aa65a82feedab63648418b24f833a20
Borys Panchenko-Vahner <nafania578@gmail.com>
2021-09-08 10:42:36 +0300

    Replace spaces with tabs

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ TESTS:
 To run tests you need specify LRLTests=TRUE while doing cmaking
 
 Purposes of tests:
-  1. Correctness
-  2. Zero-cost: same binary code for code with and without library syntax sugar
+	1. Correctness
+	2. Zero-cost: same binary code for code with and without library syntax sugar
 
 Tested environments(CMake):
-  * Win 10, MinGW 64 (x86_64-8.1.0-win32-seh-rt_v6-rev0)
-  * Debian, GCC 8.3.0, G++ 8.3.0, native make
-  //Hoping this list will include much more envs
+	* Win 10, MinGW 64 (x86_64-8.1.0-win32-seh-rt_v6-rev0)
+	* Debian, GCC 8.3.0, G++ 8.3.0, native make
+	//Hoping this list will include much more envs
--- include/lrl/iterators/iterators.hpp
+++ include/lrl/iterators/iterators.hpp
@@ -4,40 +4,40 @@
 
 namespace lrl
 {
-    namespace iterators
-    {
+	namespace iterators
+	{
 #if __cplusplus >= 201402L //cpp 14
 
-        template <typename T>
-        struct begin_iterator
-        {
-            constexpr static decltype(auto) begin(T&& container)
-            {
-                return (std::begin(std::forward<T>(container)));
-            }
-        };
+		template <typename T>
+		struct begin_iterator
+		{
+			constexpr static decltype(auto) begin(T&& container)
+			{
+				return (std::begin(std::forward<T>(container)));
+			}
+		};
 
-        template <typename T>
-        struct end_iterator
-        {
-            constexpr static decltype(auto) end(T&& container)
-            {
-                return (std::end(std::forward<T>(container)));
-            }
-        };
+		template <typename T>
+		struct end_iterator
+		{
+			constexpr static decltype(auto) end(T&& container)
+			{
+				return (std::end(std::forward<T>(container)));
+			}
+		};
 
-        template <typename T>
-        constexpr decltype(auto) begin(T&& container)
-        {
-            return (begin_iterator<T>::begin(std::forward<T>(container)));
-        }
+		template <typename T>
+		constexpr decltype(auto) begin(T&& container)
+		{
+			return (begin_iterator<T>::begin(std::forward<T>(container)));
+		}
 
-        template <typename T>
-        constexpr decltype(auto) end(T&& container)
-        {
-            return (end_iterator<T>::end(std::forward<T>(container)));
-        }
+		template <typename T>
+		constexpr decltype(auto) end(T&& container)
+		{
+			return (end_iterator<T>::end(std::forward<T>(container)));
+		}
 
 #endif
-    }
+	}
 }
