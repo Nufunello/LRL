9f505f478310b725e52db67c66b8d2bb17005265
Borys Panchenko-Vahner <nafania578@gmail.com>
2021-09-29 20:36:26 +0300

    Put tests out of anon namespace

--- Tests/src/iterators/end.cpp
+++ Tests/src/iterators/end.cpp
@@ -5,21 +5,17 @@
 #include "lrl/tests/types.hpp"
 #include "lrl/iterators/iterators.hpp"
 
-namespace
+TEST(iterators, EndReturn)
 {
-	TEST(iterators, EndReturn)
-	{
-		using vector_type = std::vector<int>;
-		constexpr bool endSameType = lrl::tests::types::isInvokeResultTypeSame<vector_type>
-		(
-			  [](auto&& vec){ return (std::end(std::forward<decltype(vec)>(vec))); }
-			, [](auto&& vec){ return (lrl::iterators::end(std::forward<decltype(vec)>(vec))); }
-		);
-		EXPECT_TRUE(endSameType)
-			<< "calls of functions \"end\" should return same type";
-
-		const vector_type vector = {1, 2, 3, 4, 5, 6};
-		EXPECT_EQ(std::end(vector), lrl::iterators::end(vector))
-			<< "calls of functions \"end\" are not equal for vector";
-	}
+	using vector_type = std::vector<int>;
+	constexpr bool endSameType = lrl::tests::types::isInvokeResultTypeSame<vector_type>
+	(
+		  [](auto&& vec){ return (std::end(std::forward<decltype(vec)>(vec))); }
+		, [](auto&& vec){ return (lrl::iterators::end(std::forward<decltype(vec)>(vec))); }
+	);
+	EXPECT_TRUE(endSameType)
+		<< "calls of functions \"end\" should return same type";
+	const vector_type vector = {1, 2, 3, 4, 5, 6};
+	EXPECT_EQ(std::end(vector), lrl::iterators::end(vector))
+		<< "calls of functions \"end\" are not equal for vector";
 }
