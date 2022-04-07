5bf0e838ce1df20b276456b3b79d64e68987986c
Borys <nafania578@gmail.com>
2022-04-08 00:08:25 +0300

    use invoke in invoke_template_functor

--- include/lrl/functional/functional.hpp
+++ include/lrl/functional/functional.hpp
@@ -38,10 +38,7 @@ namespace lrl
 		template <template <typename...> typename Functor, typename... Args>
 		constexpr decltype(auto) invoke_template_functor(Args&& ...arguments)
 		{
-			return 
-			(
-				Functor<std::decay_t<Args>...>{}(std::forward<Args>(arguments)...)
-			);
+			return functional::invoke(Functor<std::decay_t<Args>...>{}, std::forward<Args>(arguments)...);
 		}
 
 #endif
