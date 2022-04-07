07ccb6a32e5d27a42c166544d2be68a3a38ed88f
Borys <nafania578@gmail.com>
2022-04-08 00:06:09 +0300

    constexpr invoke for functors, members, methods

--- include/lrl/functional/functional.hpp
+++ include/lrl/functional/functional.hpp
@@ -1,12 +1,39 @@
 #pragma once
 
 #include <functional>
+#include  <type_traits>
 
 namespace lrl
 {
 	namespace functional
 	{
 #if __cplusplus >= 201703L
+
+		template <typename Functor, typename... Args>
+		constexpr decltype(auto) invoke(Functor&& functor, Args&& ...arguments)
+		{
+			return functor(std::forward<Args>(arguments)...);
+		}
+		template <typename Class, typename Type>
+		constexpr decltype(auto) invoke(Type (Class::*member), Class& object)
+		{
+			return (object.*member);
+		}
+		template <typename Class, typename Type>
+		constexpr decltype(auto) invoke(Type (Class::*member), const Class& object)
+		{
+			return (object.*member);
+		}
+		template <typename Class, typename Type, typename... Args, typename... Argss>
+		constexpr decltype(auto) invoke(Type (Class::*method)(Args...), Class& object, Argss&& ...args)
+		{
+			return (object.*method)(std::forward<Argss>(args)...);
+		}
+		template <typename Class, typename Type, typename... Args, typename... Argss>
+		constexpr decltype(auto) invoke(Type (Class::*method)(Args...) const, const Class& object, Argss&& ...args)
+		{
+			return (object.*method)(std::forward<Argss>(args)...);
+		}
 
 		template <template <typename...> typename Functor, typename... Args>
 		constexpr decltype(auto) invoke_template_functor(Args&& ...arguments)
